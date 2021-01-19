4fe2bee0b2c3a8a6d348a4eb72193a8ff966164e
Hannah Williams <hannahdwilliams27@gmail.com>
2021-01-19 17:58:31 -0500

    filling files

---
+++ src/components/location/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Lodation = () => {
+    return (
+        <div className="location_wrapper"> 
+            
+            <iframe
+                src="link.com"
+
+
+
+                width="100%"
+                height="500px"
+                frameBorder="0"
+                allowFullScreen
+                title="myframe"
+            ></iframe>
+            
+            <div className="location_tag"> 
+                <div>Location</div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Location;
